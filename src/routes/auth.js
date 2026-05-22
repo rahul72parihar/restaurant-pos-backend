01aece27d92e55a95917aee79dd52e5d7d643863
Rahul Parihar <rahul72parihar@gmail.com>
2026-05-22 11:15:40 +0530

    prisma fix 2

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-import { prisma } from "../../prisma/prisma";
+import { prisma } from "../utils/prisma";
 const { authenticate } = require("../middleware/auth");
 
 
--- src/utils/prisma.js
+++ src/utils/prisma.js
@@ -1,11 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+const { PrismaClient } = require("@prisma/client");
 
 const globalForPrisma = global;
 
-export const prisma =
+const prisma =
   globalForPrisma.prisma ||
   new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
+
+module.exports = prisma;
